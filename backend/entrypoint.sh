337f6b97dd29ce4c1e055a38e9d20d17fc8835fd
Abinaya-Grd <23bct002@grd.edu.in>
2026-08-08 21:11:27 +0530

    Refactor entrypoint.sh to remove MySQL wait
    Removed MySQL wait logic and added Django startup message.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for MySQL at $DB_HOST:$DB_PORT..."
-while ! nc -z "$DB_HOST" "$DB_PORT"; do
-  sleep 1
-done
-echo "MySQL is up."
+echo "Starting Django..."
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
